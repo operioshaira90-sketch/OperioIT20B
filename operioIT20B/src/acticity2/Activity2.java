9f7c17d293c4ba4a7fc44834bd215806c71100d7
Shaira Operio <operioshaira90@gmail.com>
2026-08-22 23:58:15 +0800

    initial commit1

---
+++ operioIT20B/src/acticity2/Activity2.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package acticity2;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Activity2 {
+    public static void main(String[] args) {
+        System.out.println("HEllo,world");
+    }
+}
---
+++ operioIT20B/src/operioit20b/OperioIT20B.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package operioit20b;
+
+/**
+ *
+ * @author ASUS
+ */
+public class OperioIT20B {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         
+    }
+    
+}
