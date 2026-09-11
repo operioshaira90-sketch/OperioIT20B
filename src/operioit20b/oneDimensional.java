3719b70e7ac82eb0dc7202c439ccbb3b6088a648
Shaira Operio <operioshaira90@gmail.com>
2026-09-12 00:01:34 +0800

    Initial commit

---
+++ src/operioit20b/oneDimensional.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package operioit20b;
+
+/**
+ *
+ * @author ASUS
+ */
+public class oneDimensional {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int[] values = {1,2,3,4,5};
+        
+        for (int i = 0; i < values.length; i++) {
+            System.out.print(values[i] + "  ");
+        }
+    }
+    
+}
