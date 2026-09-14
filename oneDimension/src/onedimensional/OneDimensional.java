fbb27d52f6101eb6360ad8b2810b1e88a250c363
Shaira Operio <operioshaira90@gmail.com>
2026-09-14 16:15:27 +0800

    Add files via upload

---
+++ oneDimension/src/onedimensional/OneDimensional.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package onedimensional;
+
+/**
+ *
+ * @author ASUS
+ */
+public class OneDimensional {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int[] values = {2, 4, 5, 6, 8};
+        
+        for (int i = 0; i < values.length; i++) {
+            System.out.print(values[i] + " ");
+        }
+    }
+    
+}
